7caae427878dc3ca861ac88e3168ea514a92b236
pj0620 <pjm7748@g.rit.edu>
2021-11-27 02:14:15 -0800

    updated menu with logo, changed favicon, changed page title

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,80 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/privacy-policy">Privacy Policy</router-link> |
-    <router-link to="/contact-info">Contact Info</router-link>
+  <Menubar ref="menuRef" :model="items" id="menu" >
+    <template #start>
+      <img @click="gotoHome()" alt="logo" src="/images/logo-light-whitebg-vertical.png" height="80" class="p-mr-2">
+    </template>
+  </Menubar>
+  <div id="content">
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#nav {
-  padding: 0px;
-}
+  #menu {
+    /* background-color: var(--color-primary); */
+    background-color: white;
+    padding: 0.5rem 0 0.3rem 1rem;
+    margin: 0 1rem 0.5rem 1rem;
+  }
 
-#nav a {
-  font-weight: bold;
-  font-size: 1.05rem;
-  color: #2c3e50;
-}
+  .p-menuitem-text {
+    font-size: 1.1rem;
+  }
 
-#nav a.router-link-exact-active {
-  color: var(--color-primary);
-}
+  .pi-bars {
+    font-size: 1.4rem !important;
+  }
+
+  .p-menubar-button {
+    right: 2rem;
+    position:absolute; 
+  }
+
+  .p-menubar-root-list {
+    right: 2rem;
+    position:absolute; 
+  }
 </style>
+
+<script setup lang="ts">
+  import Menubar from 'primevue/menubar';
+import { ref, watch } from 'vue';
+  import { useRouter } from 'vue-router';
+
+  const router = useRouter();
+
+  // Refs
+  const menuRef = ref(null);
+
+  const items = [
+    {
+      label:'Home',
+      icon:'pi pi-fw pi-home',
+      command:() => router.push('/')
+    },
+    {
+      label:'Contact Info',
+      icon:'pi pi-fw pi-envelope',
+      command:() => router.push('/contact-info')
+    },
+    {
+      label:'Privacy Policy',
+      icon:'pi pi-fw pi-lock',
+      command:() => router.push('/privacy-policy')
+    },
+  ]
+
+  const gotoHome = () => router.push('/');
+
+  watch(menuRef, (currentValue, oldValue) => {
+    console.log(menuRef.value);
+  });
+</script>
